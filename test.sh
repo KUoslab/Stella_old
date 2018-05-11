888193b61e6d3ae068f01eac7fa2c1f55790b883
root <master@os.korea.ac.kr>
2018-05-11 15:27:58 +0900

    modity test.sh script

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-./insert_target_vm.sh VM_1 b_bw 1024
-./insert_target_vm.sh VM_2 b_bw 1024
+./insert_target_vm.sh VM_1 b_bw 200
+./insert_target_vm.sh VM_2 b_bw 400
 #./insert_target_vm.sh vm3 c_usage 10
 #./insert_target_vm.sh vm4 c_usage 30
 #./insert_target_vm.sh vm5 c_usage 35
